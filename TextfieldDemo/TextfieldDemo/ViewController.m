260fa7978f92e486d7fa182b7899a37e6b73a218
herodon <dujie0508@microwu.com>
2016-12-16 15:46:44 +0800

    commit

--- TextfieldDemo/TextfieldDemo/ViewController.m
+++ TextfieldDemo/TextfieldDemo/ViewController.m
@@ -12,7 +12,9 @@
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *bottomSpace;
 @property (weak, nonatomic) IBOutlet UITextField *textField;
 @property (weak, nonatomic)  UITextField *firstResponseTF;
+@property (weak, nonatomic) IBOutlet UITextField *textfieldTop;
 
+@property (assign, nonatomic)  BOOL hideInner;
 @end
 
 @implementation ViewController
@@ -20,46 +22,55 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.textField.delegate=self;
-    self.textField.tag=103;
+    self.textfieldTop.delegate=self;
+    
+    
+    self.firstResponseTF=self.textField;
+    
     //简历通知
     [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(keyboardWillChangeFrameNotification:) name:UIKeyboardWillChangeFrameNotification object:nil];
     self.bottomSpace.constant=-50;
-    
 }
 - (IBAction)clickAction:(id)sender {
     [self.textField becomeFirstResponder];
 }
 -(void)keyboardWillChangeFrameNotification:(NSNotification *)note{
+    if (!self.hideInner) {
+        return;
+    }
     //获取键盘的饿frame
     CGRect frmae = [note.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue];
-    NSLog(@"%@", note);
+    
     //让TextFiled的底部约束间距为屏幕高度减去键盘顶部的y值即可
     //注意 这里不要使其等于键盘的高度，因为高度时死的，会导致键盘下去后，TextField并未下去的结果。
-    if (self.firstResponseTF.tag == self.textField.tag) {
-        self.bottomSpace.constant = [UIScreen mainScreen].bounds.size.height - frmae.origin.y;
+    self.bottomSpace.constant = [UIScreen mainScreen].bounds.size.height - frmae.origin.y;
+    
+    //获取键盘的动画时间，使TextField与键盘的形态一致
+    CGFloat interval = [note.userInfo[UIKeyboardAnimationDurationUserInfoKey] doubleValue];
+    //设置Text的动画
+    [UIView animateWithDuration:interval animations:^{
         
-        //获取键盘的动画时间，使TextField与键盘的形态一致
-        CGFloat interval = [note.userInfo[UIKeyboardAnimationDurationUserInfoKey] doubleValue];
-        //设置Text的动画
-        [UIView animateWithDuration:interval animations:^{
-            
-            //注意这里不是改变值，之前已经改变值了，
-            //在这里需要做的事强制布局
-            [self.view layoutIfNeeded];
-            
-        }];
-    }
+        //注意这里不是改变值，之前已经改变值了，
+        //在这里需要做的事强制布局
+        [self.view layoutIfNeeded];
+        
+    }];
+    
     
 }
+
 - (BOOL)textFieldShouldBeginEditing:(UITextField *)textField{
-    self.firstResponseTF = textField;
+    if (textField==self.firstResponseTF) {
+        self.hideInner=YES;
+    }
+    else
+    {
+        self.hideInner=NO;
+    }
+
     return YES;
 }
 
-- (BOOL)textFieldShouldReturn:(UITextField *)textField{
-    self.firstResponseTF = nil;
-    return YES;
-}
 
 
 -(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
